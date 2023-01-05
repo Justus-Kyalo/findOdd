86f9be6a786d01e0bad7d49e205fe1252a0af3ac
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-05 16:34:27 +0300

    solved

---
+++ main.js
@@ -0,0 +1,24 @@
+function findOdd(a) {
+  // Initialize an object to store the counts of each number
+  let counts = {};
+
+  // Iterate over the array and count the frequency of each number
+  for (const num of a) {
+    if (counts[num]) {
+      counts[num]++;
+    } else {
+      counts[num] = 1;
+    }
+  }
+
+  // Iterate over the counts and return the first number whose frequency is odd
+  for (const num in counts) {
+    if (counts[num] % 2 !== 0) {
+      // Parse the number as an integer before returning it
+      return parseInt(num);
+    }
+  }
+
+  // Return null if all the frequencies are even
+  return null;
+}
